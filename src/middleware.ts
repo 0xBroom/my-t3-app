e723725f97d6558d66ab8062ac44917a34195268
0xbroom <juantaban@gmail.com>
2023-05-19 22:50:05 +0200

    feat[auth]: add signin and user buttons

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,9 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { withClerkMiddleware } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-export default authMiddleware();
+export default withClerkMiddleware(() => {
+  return NextResponse.next();
+});
 
-export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/"],
-};
+// Stop Middleware running on static files
+export const config = { matcher:  '/((?!_next/image|_next/static|favicon.ico).*)',};
