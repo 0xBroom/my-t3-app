7e7105ebd746f06262eb69f67c238d7bca50483e
0xbroom <juantaban@gmail.com>
2023-05-19 23:45:22 +0200

    feat: store and show posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,9 +6,10 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
 
   const user = useUser();
+
+  const { data } = api.posts.getAll.useQuery()
 
   return (
     <>
@@ -19,8 +20,13 @@ const Home: NextPage = () => {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div>
-          {!user.isSignedIn && <SignInButton/>}
-          {!!user.isSignedIn && <UserButton/>}
+          {!user.isSignedIn && <SignInButton />}
+          {!!user.isSignedIn && <UserButton />}
+        </div>
+        <div>
+          {data?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
         </div>
       </main>
     </>
