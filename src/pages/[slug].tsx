55e16b3d882bac268a939ed35e32adab266b7065
0xbroom <juantaban@gmail.com>
2023-05-28 19:56:04 +0200

    feat: create basic user profile.
    - Using ssg helper for static server side rendering.

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,31 +1,74 @@
-import { type NextPage } from "next";
+import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
 import { api } from "~/utils/api";
+import Image from "next/image";
 
-const ProfilePage: NextPage = () => {
+const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
 
-  const { data, isLoading } = api.profile.getUserByUsername.useQuery({
-    username: "estebaner",
+  const { data } = api.profile.getUserByUsername.useQuery({
+    username,
   });
-  if (isLoading) return <div>Loading...</div>;
 
   if (!data) return <div>404</div>;
 
   return (
     <>
       <Head>
-        <title>Profile</title>
+        <title>{data.username}</title>
       </Head>
-      <main className="flex h-screen justify-center">
-        <div>
-          {data.username}
+      <PageLayout>
+        <div className="relative h-48 bg-slate-600">
+          <Image
+            src={data.profileImageUrl}
+            alt={`${data.username ?? ""}'s profile pic`}
+            width={320}
+            height={320}
+            className="absolute bottom-0 left-0 -mb-[80px] ml-4 rounded-full border-4 border-black bg-black w-40 h-40 "
+          />
         </div>
-      </main>
+        <div className="h-[80px]"></div>
+
+        <div className="p-6 text-2xl font-bold">
+          {`@${data.username ?? ""}`}
+        </div>
+        <div className="w-full border-b border-slate-400" />
+      </PageLayout>
     </>
   );
 };
 
-import { createProxySSGHelpers } from '@trpc/react-query/ssg';
+import { createServerSideHelpers } from '@trpc/react-query/server';
+import { appRouter } from "~/server/api/root";
+import { prisma } from "~/server/db";
+import SuperJSON from "superjson";
+import { PageLayout } from "~/components/layout";
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const ssg = createServerSideHelpers({
+    router: appRouter,
+    ctx: { prisma, userId: null },
+    transformer: SuperJSON, // optional - adds superjson serialization
+  });
+
+  const slug = context.params?.slug;
+
+  if (typeof slug !== "string") throw new Error("no slug");
+
+  const username = slug.replace("@", "");
+
+  await ssg.profile.getUserByUsername.prefetch({ username });
+
+  return {
+    props: {
+      trpcState: ssg.dehydrate(),
+      username
+    },
+  }
+};
+
+export const getStaticPaths = () => {
+  return { paths: [], fallback: "blocking" }
+}
 
 
 export default ProfilePage;
